c93283a86e98eb0d4543f9de93c95fc7545b3b39
Tully Clark <tullyclark96@gmail.com>
2019-10-25 05:14:39 +1100

    implement view runs

--- site/services/query_services.py
+++ site/services/query_services.py
@@ -1,6 +1,6 @@
 from services.select_user_data import select_user_data
 from services.save_services import insert_user_data
-from data.source import DataView
+from data.source import DataView, ViewRun
 from data.db_session import create_session
 
 ##get linked sessions
@@ -11,6 +11,7 @@ def run_query(id, out):
 	.all()
 
 	session.close()
+
 	data=[]
 	try:
 		data = select_user_data(id)
@@ -18,8 +19,18 @@ def run_query(id, out):
 		print(str(error))
 
 	if out == 'write':
-		for d in data:
-			for data_view in views:
-				insert_user_data(d, data_view.id)
+		for data_view in views:
+
+			session = create_session()
+
+			view_run = ViewRun()
+			session.add(view_run)
+			view_run.data_view_id = data_view.id
+			session.commit()
+
+			for d in data:
+				insert_user_data(d, view_run.id)
+			
+			session.close()
 	else:
 		return(data)
