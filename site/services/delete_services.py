a2eab735641ba7fa02bb2b9b569e061fa43ad2e1
Tully Clark <tullyclark96@gmail.com>
2019-10-25 05:13:39 +1100

    Update delete_services.py

--- site/services/delete_services.py
+++ site/services/delete_services.py
@@ -13,15 +13,18 @@ def delete_object(id, item_type):
         .first()
     if item_type == ScheduleStep:   
         delete_cron_job(obj.schedule_id)
+        session.commit()
+        write_cron_job(obj.schedule_id)
     session.delete(obj)
     session.commit()
-    write_cron_job(obj.schedule_id)
     session.close()
 
 def drop_view(id):
     session = db_session.create_session()
 
-    print(obj.name)
+    obj = session.query(DataView) \
+        .filter_by(id=id) \
+        .first()
 
     sql_text = f"drop view {obj.view_name}"
     session.execute(sql_text)
