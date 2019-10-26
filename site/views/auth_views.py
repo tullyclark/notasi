0f3adab1db0eee3e8709f0290273a47626855e32
Tully Clark <tullyclark96@gmail.com>
2019-10-26 17:38:11 +1100

    Update auth_views.py

--- site/views/auth_views.py
+++ site/views/auth_views.py
@@ -74,7 +74,7 @@ def saml_client_for(idp_name=None):
                 'want_response_signed': False,
             },
         },
-        "xmlsec_binary" : "/usr/local/bin/xmlsec1"
+        "xmlsec_binary" : "/usr/lib/x86_64-linux-gnu/libxmlsec1"
     }
     spConfig = Saml2Config()
     spConfig.load(settings)
