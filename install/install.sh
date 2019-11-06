23171993c017e0178cc57a3dd769cf6c9da7adb0
Tully Clark <tullyclark96@gmail.com>
2019-11-06 22:39:52 +1100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -11,6 +11,7 @@ fi
 
 #get dependancies
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
 apt-get install python3-pip libxmlsec1-dev pkg-config virtualenv uwsgi nginx postgresql postgresql-server-dev-10 libmysqlclient-dev unixodbc-dev
 ACCEPT_EULA=Y apt-get install msodbcsql17
