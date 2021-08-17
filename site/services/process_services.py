76aad135bfa7dc4ce3a6e9feac31a3b215e2b1f1
tullyclark <tullyclark96@gmail.com>
2021-08-18 09:37:25 +1000

    More pip

--- site/services/process_services.py
+++ site/services/process_services.py
@@ -27,6 +27,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
+from bs4 import BeautifulSoup
 
 def default(o):
 	if isinstance(o, (datetime.date, datetime.datetime)):
